61743713b3a8d6f05bdaa3b588c6cdc39dada838
danielepo <dnl.pozzobon@gmail.com>
2016-08-11 14:42:38 +0200

    Create progect

---
+++ compile.js
@@ -0,0 +1,14 @@
+var fs = require("fs");
+var Handlebars = require("handlebars");
+
+var path = "./";
+
+function render() {
+    var resume = require("./europass-cv-it.json");
+
+    var template = fs.readFileSync(path + "./template.hbs", "utf-8");
+    var hb = Handlebars.compile(template);
+    var html = hb({ resume: resume });
+    fs.writeFileSync("./index.html", html);
+}
+render();
