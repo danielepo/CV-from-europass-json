211c755def40fa5e1f5f82dc7be43036de4abe6a
danielepo <dnl.pozzobon@gmail.com>
2016-08-23 15:55:48 +0200

    Fix date and some adjustments

--- compile.js
+++ compile.js
@@ -1,7 +1,26 @@
 var fs = require("fs");
 var Handlebars = require("handlebars");
+var moment = require("moment");
 
 var path = "./";
+
+var DateFormats = {
+    data: "DD MMMM YYYY",
+    mese: "MMMM - YYYY"
+};
+
+Handlebars.registerHelper("formatDate", function(date) {
+    moment.locale("it");
+    if (moment) {
+        // can use other formats like 'lll' too
+        if (date.Day) {
+            return moment(date.Year + "-" + date.Month + "/" + date.Day).format("DD MMM YYYY");
+        }
+        return moment(date.Year + "-" + date.Month + "/1").format("MMM YYYY");
+    } else {
+        return datetime;
+    }
+});
 
 function render() {
     var resume = require("./europass-cv-it.json");
